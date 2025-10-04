fc198bbe81bda75c0bc4f0e90894fc05b8aa8559
Wafa Adam <wafaadam548@gmail.com>
2025-10-04 11:00:50 -0400

    learn make space between words

--- app.cpp
+++ app.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+using namespace std;
 int main() { 
-  std::cout <<"I am here";
+cout <<"My name is wafa"<<endl;
+cout <<"sara ";
   return 0;
 }
+//if you wanna make space between tow lines youu have to=wo waus
+//cout<<endl; or after th eword like cout<<my name us wafa<<endl;
+//every thing inside the pracets }{}
