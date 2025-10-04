ee6c00b2d477cdff380884f7cc814902db73b4e0
Wafa Adam <wafaadam548@gmail.com>
2025-10-04 11:14:51 -0400

    add \n

--- app.cpp
+++ app.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 int main() { 
 cout <<"My name is wafa"<<endl;
-cout <<"sara ";
+cout <<"sara\n ";
+cout<<"nchbvhfbh";
+
   return 0;
 }
 //if you wanna make space between tow lines youu have to=wo waus
 //cout<<endl; or after th eword like cout<<my name us wafa<<endl;
 //every thing inside the pracets }{}
+// there is anothere way by writing /n but this one is inside the ""
