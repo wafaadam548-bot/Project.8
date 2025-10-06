b84c36541cda544b5e30dff1e483e2a572ecb88b
Wafa Adam <wafaadam548@gmail.com>
2025-10-06 17:47:18 -0400

    tried beautiful calculate

--- index.py
+++ index.py
@@ -1,3 +1,11 @@
 #first time for me learn python lang
 #I will win the ipad and lean more about progremming because that is my drean
 #str()lett the int to sstring
+print("Welcom to our Calcolater app")
+num1=float(input("Enter number 1"))
+num2=float(input("Enter number2"))
+print(num1+num2)
+print(num1-num2)
+print(num1/num2)
+print(num1*num2)
+print(num1%num2)
