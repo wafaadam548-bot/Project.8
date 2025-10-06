308f53572f969d8661e6a203e31c8859f1921e2c
Wafa Adam <wafaadam548@gmail.com>
2025-10-06 19:32:16 -0400

    make a small calculater

--- index.py
+++ index.py
@@ -3,9 +3,24 @@
 #str()lett the int to sstring
 print("Welcom to our Calcolater app")
 num1=float(input("Enter number 1 :"))
+oper=input("Choose an operation(+,-,*,/,%)")
 num2=float(input("Enter number2 :"))
-print("Add :"+str(num1+num2))
-print("Sub :"+str(num1-num2))
-print("Div :"+str(num1/num2))
-print("Mult"+str(num1*num2))
-print("Mosc"+str(num1%num2))
+if oper=="+":
+    result=num1+num2
+    print(num1,"+" ,num2,"=" ,result)
+elif oper=="-":
+    result=num1-num2
+    print(num1,"-", num2,"=",result)
+elif oper=="*":
+    result=num1*num2
+    print(num1,"*",num2,"=",result)
+elif oper=="/":
+    result=num1/num2    
+    print(num1,"/",num2,"=",result)
+elif oper=="%":
+    result=num1%num2
+    print(num1,"%",num2,"=",result)
+else :
+    print("Add"+str(num1+num2),"Sub"+str(num1-num2),"Mult"+str(num1*num2),"Div"+str(num1/num2),"Mod"+str(num1%num2))
+    
+   
