136087b9d91be3574fe53f8a38799bfd4f4f199e
Wafa Adam <wafaadam548@gmail.com>
2025-10-06 18:19:34 -0400

    simple calcolater

--- index.py
+++ index.py
@@ -2,10 +2,10 @@
 #I will win the ipad and lean more about progremming because that is my drean
 #str()lett the int to sstring
 print("Welcom to our Calcolater app")
-num1=float(input("Enter number 1"))
-num2=float(input("Enter number2"))
-print(num1+num2)
-print(num1-num2)
-print(num1/num2)
-print(num1*num2)
-print(num1%num2)
+num1=float(input("Enter number 1 :"))
+num2=float(input("Enter number2 :"))
+print("Add :"+str(num1+num2))
+print("Sub :"+str(num1-num2))
+print("Div :"+str(num1/num2))
+print("Mult"+str(num1*num2))
+print("Mosc"+str(num1%num2))
