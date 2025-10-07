b8f24e3b940643ab90de6edd4c551b980bd56870
Wafa Adam <wafaadam548@gmail.com>
2025-10-07 05:24:34 -0400

    I am trying to make t more organayzed

--- Unit Converter .py
+++ Unit Converter .py
@@ -1,8 +1,13 @@
 print("Hellow too our Unit Converter web")
-num1=str(input("please choose(wight,hight,Tembreture,Time)"))
-print(num1)
-num2= str(input("please choose(g to kg),(kg to g),(g to lp)(kg to ;lp),(cm to m),(m to cm),(m to km),(cm to km),(km to m),(km to cm),(m/s to km/h),(km/h to m/h),(ms to knots),(c to f),(f to c),(sec to min),(min to sec),(hours to min ),(min to hours)"))
-print(num2)
-num3= float(input("Please add your number"))
-print(num3)
+print("Choose what you want to convert:")
+print("1-Weight")
+print("2-height")
+print("3-Temperature")
+print("4-Time")
+print(input("please Enter your Converter "))
 
+    
+    
+    
+    
+    
