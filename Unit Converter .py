a5d32955bdaf1f38bc0c7ebf9dd54d65ecc6305b
Wafa Adam <wafaadam548@gmail.com>
2025-10-06 22:30:44 -0400

    making Unit Converter

---
+++ Unit Converter .py
@@ -0,0 +1,8 @@
+print("Hellow too our Unit Converter web")
+num1=str(input("please choose(wight,hight,Tembreture,Time)"))
+print(num1)
+num2= str(input("please choose(g to kg),(kg to g),(g to lp)(kg to ;lp),(cm to m),(m to cm),(m to km),(cm to km),(km to m),(km to cm),(m/s to km/h),(km/h to m/h),(ms to knots),(c to f),(f to c),(sec to min),(min to sec),(hours to min ),(min to hours)"))
+print(num2)
+num3= float(input("Please add your number"))
+print(num3)
+
