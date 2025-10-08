347d2b05fcb1f42aa94e91f28a63520853e6d81c
Wafa Adam <wafaadam548@gmail.com>
2025-10-07 20:25:54 -0400

    try to make main py is the main page

---
+++ main.py
@@ -0,0 +1,16 @@
+print("==================================="
+"------------------------------------")
+print("Welcome to out smart Calculation Project!!")
+print(" Please Choose What you want to use")
+print("1. Baisec Calculator")
+print("2. BMI Calculator")
+print("3. Unit Converter")
+choise=input("Enter Your choise(1,2,3)")
+if choise=="1":
+    import Calcolater
+elif choise=="2":
+    import BMI
+elif choise=="3":
+    import Unit
+else:
+    print("Sorry, make sure you follow the steps and write with the same letters and spelling.")            
