b388cbed8507f3882195cfa0115e1e6887c8501d
Wafa Adam <wafaadam548@gmail.com>
2025-10-09 16:20:57 -0400

    delete the main  exe things to make new one after I make new file

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ print(" Please Choose What you want to use")
 print("1. Baisec Calculator")
 print("2. BMI Calculator")
 print("3. Unit Converter")
+print("4. Currency conversion")
 choise=input("Enter Your choise(1,2,3)")
 if choise=="1":
     import Calcolater
@@ -12,6 +13,8 @@ elif choise=="2":
     import BMI
 elif choise=="3":
     import Unit
+elif choise=="4":
+    import Unit    
 else:
     print("Sorry, make sure you follow the steps and write with the same letters and spelling.")            
 print("Please Press Enter to Exit the page")
