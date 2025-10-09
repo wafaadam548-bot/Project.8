ffd3bc4da01ff310ee24f61971b7b2a37f434bbe
Wafa Adam <wafaadam548@gmail.com>
2025-10-09 16:53:50 -0400

    add while loop to the code

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-print("==================================="
-"------------------------------------")
-print("Welcome to out smart Calculation Project!!")
-print(" Please Choose What you want to use")
-print("1. Baisec Calculator")
-print("2. BMI Calculator")
-print("3. Unit Converter")
-print("4. Currency conversion")
-choise=input("Enter Your choise(1,2,3,4) : ")
-if choise=="1":
-    import Calcolater
-elif choise=="2":
-    import BMI
-elif choise=="3":
-    import Unit
-elif choise=="4":
-    import Currency    
-else:
-    print("Sorry, make sure you follow the steps and write with the same letters and spelling.")            
-print("Please Press Enter to Exit the page")
+while True:
+    print("==================================="
+    "------------------------------------")
+    print("Welcome to out smart Calculation Project!!")
+    print(" Please Choose What you want to use")
+    print("1. Baisec Calculator")
+    print("2. BMI Calculator")
+    print("3. Unit Converter")
+    print("4. Currency conversion")
+    choise=input("Enter Your choise(1,2,3,4) : ")
+    if choise=="1":
+        import Calcolater
+    elif choise=="2":
+        import BMI
+    elif choise=="3":
+        import Unit
+    elif choise=="4":
+        import Currency    
+    else:
+        print("Sorry, make sure you follow the steps and write with the same letters and spelling.")            
+    print("Please Press Enter to Exit the page")
+    again = input("Do you want to use SmartCalc again? (yes/no): ")
+    if again.lower() != "yes":
+        print("Thank you for using SmartCalc!") 
+        break
