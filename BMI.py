32a56b9ab51d0f73cfc1d8be1ab0d83949a6a521
Wafa Adam <wafaadam548@gmail.com>
2025-10-10 07:05:20 -0400

    try to add function

--- BMI.py
+++ BMI.py
@@ -1,20 +1,22 @@
 print("Hello in our BMI web")
 print("Lets calcoulate your BMI")
-Tall=float(input("Please Enter Your Hight in Cm :"))
-print("The hight is "+ str (Tall))
+def BMI():
+    Tall=float(input("Please Enter Your Hight in Cm :"))
+    print("The hight is "+ str (Tall))
 
-wight=float(input("Plese Enter your wight(kg) :"))
-print("The wight is "+ str (wight))
-Tall=Tall/100
-result=wight/Tall**2
-print("your BMI is :"+ str (result))
-if result<=18.5:
-    print("UnderWight")
-elif 18.5<= result<=24.5:
-    print("Healthy")
-elif 24.5<=result<= 29.9:
-    print("OverWight")
-elif 29.9<=result :
-    print("Obesity")    
-else:
-    print("Sorry try again")
+    wight=float(input("Plese Enter your wight(kg) :"))
+    print("The wight is "+ str (wight))
+    Tall=Tall/100
+    result=wight/Tall**2
+    print("your BMI is :"+ str (result))
+    if result<=18.5:
+        print("UnderWight")
+    elif 18.5<= result<=24.5:
+        print("Healthy")
+    elif 24.5<=result<= 29.9:
+        print("OverWight")
+    elif 29.9<=result :
+        print("Obesity")    
+    else:
+        print("Sorry try again")
+BMI()        
